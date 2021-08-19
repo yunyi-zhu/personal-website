cac0bd07d8c9828b50d6f48031510f2be2a5c367
Yunyi Zhu <yunyizhu@mit.edu>
2021-08-19 01:26:14 -0400

    phd update

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,34 +10,14 @@ const About = () => (
         Yunyi Zhu
       </div>
       <div className="paragraph">
-        MIT Master's student
-        <div>
-          <a href="http://catalog.mit.edu/degree-charts/computer-science-engineering-course-6-3/" target="_blank">Computer Science and Engineering</a>
-        </div>
-        <div>
-          minor in <a href="http://architecture.mit.edu/architecture-and-urbanism/degree/minor-design" target="_blank">Design</a>
-        </div>
+        PhD student in the <a href="https://hcie.csail.mit.edu/" target="_blank">HCI Engineering Group</a> at MIT, advised
+        by <a href="https://hcie.csail.mit.edu/stefanie-mueller" target="_blank">Prof. Stefanie Mueller</a>.
       </div>
       <div className="paragraph">
         <div className="semibold">
           Email:
         </div>
         <div>yunyizhu@mit.edu</div>
-      </div>
-      <div className="paragraph">
-        <div className="semibold">
-          Prior Supervisors:
-        </div>
-        <div>
-          <a href="https://hcie.csail.mit.edu/stefanie-mueller.html" target="_blank">
-            Prof. Stefanie Mueller
-          </a>
-        </div>
-        <div>
-          <a href="https://people.csail.mit.edu/dnj/" target="_blank">
-            Prof. Daniel Jackson
-          </a>
-        </div>
       </div>
       <div className="paragraph">
         <div className="semibold">
@@ -57,7 +37,7 @@ const About = () => (
       </a> and <a href="https://http://people.csail.mit.edu/dnj/" target="_blank">
         Prof. Daniel Jackson
       </a> at <a href="https://www.csail.mit.edu/" target="_blank">MIT CSAIL</a>.
-        I won the <a href="https://www.eecs.mit.edu/news-events/announcements/best-best-superurop-2018-2019-award-winners" target="_blank">MIT EECS best undergraduate research award</a> in 2019.
+        I won the <a href="https://www.eecs.mit.edu/news-events/announcements/best-best-superurop-2018-2019-award-winners" target="_blank">MIT EECS best undergraduate research award</a> in 2019 and 2021.
       </div>
     </div>
   </div>
--- src/components/conference/conference-attendance.js
+++ src/components/conference/conference-attendance.js
@@ -8,7 +8,10 @@ const Attendance = () =>
       </div>
       <ul className="conference-attendance">
         <li>
-          <a href="https://chi2020.acm.org/" target="_blank">ACM Conference on Human Factors in Computing Systems (CHI) 2020, Virtual</a>
+          <a href="https://chi2021.acm.org/" target="_blank">ACM Conference on Human Factors in Computing Systems (CHI) 2021, Virtual</a>
+        </li>
+        <li>
+          <a href="https://uist.acm.org/uist2020/" target="_blank">Symposium on User Interface Software and Technology (UIST) 2020, Virtual</a>
         </li>
         <li>
           <a href="https://uist.acm.org/uist2019/" target="_blank"> Symposium on User Interface Software and Technology (UIST) 2019, New Orleans</a>
