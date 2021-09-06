c5d59e1bd000028642ea185de3ee52f1187f7cb2
Yunyi Zhu <yunyizhu@mit.edu>
2021-09-05 22:52:03 -0400

    add back the woodworking pages

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import WoodworkingAboutPage from './woodworking/woodworking-about-page';
 import WoodworkingProjectPage from './woodworking/woodworking-project-page';
 import ProjectsPage from './pages/projects-page';
 
-/* eslint-disable import/no-webpack-loader-syntax */
-import One_ShoeBox from '!babel-loader!@mdx-js/loader!./woodworking/woodworking-pages/1_shoe-box.mdx';
-
-
 const App = () => {
   return (
     <Router forceRefresh={true}>
@@ -26,8 +22,7 @@ const App = () => {
           <WoodworkingAboutPage />
         </Route>
         <Route path="/woodworking/project/:id">
-          {/*<WoodworkingProjectPage />*/}
-          <One_ShoeBox />
+          <WoodworkingProjectPage />
         </Route>
       </Switch>
     </Router>
--- src/woodworking/woodworking-project-page.js
+++ src/woodworking/woodworking-project-page.js
@@ -4,11 +4,11 @@ import Footer from './woodworking-footer';
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 
-/* eslint-disable import/no-webpack-loader-syntax */
-// import One_ShoeBox from '!babel-loader!@mdx-js/loader!./woodworking-pages/1_shoe-box.mdx';
+// /* eslint-disable import/no-webpack-loader-syntax */
+// // import One_ShoeBox from '!babel-loader!@mdx-js/loader!./woodworking-pages/1_shoe-box.mdx';
 
 const links = {
-  // "1_shoe-box": <One_ShoeBox />
+  "1_shoe-box": <One_ShoeBox />
 }
 
 
@@ -18,9 +18,9 @@ const Project = () => {
     <div className="woodworking-page">
       <Navbar />
       <div className="woodworking-project-page">
-        {/*{!!id && links[id] &&*/}
-        {/*  links[id]*/}
-        {/*}*/}
+        {!!id && links[id] &&
+          links[id]
+        }
         {!id || !links[id] &&
         <div>
           <div>
