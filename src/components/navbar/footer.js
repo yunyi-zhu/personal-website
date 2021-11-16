9b8c34aed8b807b90a905b067ab7a96429880332
Yunyi Zhu <yunyizhu@mit.edu>
2021-11-15 20:19:54 -0500

    add link to woodworking site

--- src/components/navbar/footer.js
+++ src/components/navbar/footer.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <div className="footer">
-    <div>2020 by Yunyi Zhu</div>
+    <div>
+      <div>2021 by Yunyi Zhu</div>
+      <div>personal interests: <a href="http://yunyizhu.info/woodworking" target="_blank">woodworking</a></div>
+    </div>
   </div>
 );
 
