1ed0159cc7074d20f11dc2881ee4d6f76b80fd0e
Yunyi Zhu <yunyizhu@mit.edu>
2022-10-29 08:23:48 +0900

    Updates

---
+++ precache-manifest.8342092503a01192ca21c054c7f44c91.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d179bd29467c9416bc096be05e3a4235",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "501b561a02cf0a1ef3f9",
+    "url": "/personal-website/static/css/main.093daf8c.chunk.css"
+  },
+  {
+    "revision": "730c243577a3c034c751",
+    "url": "/personal-website/static/js/2.99ea1c15.chunk.js"
+  },
+  {
+    "revision": "501b561a02cf0a1ef3f9",
+    "url": "/personal-website/static/js/main.5267c27a.chunk.js"
+  },
+  {
+    "revision": "ba6a642543421706c3a8",
+    "url": "/personal-website/static/js/runtime-main.0d16f302.js"
+  },
+  {
+    "revision": "32f5bedff4b87778788d6705b0868083",
+    "url": "/personal-website/static/media/1_shoe-box.32f5bedf.jpg"
+  },
+  {
+    "revision": "1e7c029bdc2dadd0dd9eabdb16d78e90",
+    "url": "/personal-website/static/media/2_shoe-rack.1e7c029b.jpg"
+  },
+  {
+    "revision": "2046f25e9eb76f8d5f22d7672d77d158",
+    "url": "/personal-website/static/media/3dprint-textile.2046f25e.jpg"
+  },
+  {
+    "revision": "98090bc0013f64408d65a0dd8b8dbdb3",
+    "url": "/personal-website/static/media/bus-stop.98090bc0.png"
+  },
+  {
+    "revision": "f3ea918bee1848c6ec75cf4fea542d06",
+    "url": "/personal-website/static/media/catology.f3ea918b.png"
+  },
+  {
+    "revision": "2fa8839e61d47845d9062cad9a179c05",
+    "url": "/personal-website/static/media/clevr.2fa8839e.png"
+  },
+  {
+    "revision": "92f233a546a0cd50fd378082d1210ea1",
+    "url": "/personal-website/static/media/curveboard-thumbnail.92f233a5.jpg"
+  },
+  {
+    "revision": "9cb1a7f9eed2d14bfabe004072c4c9cd",
+    "url": "/personal-website/static/media/dejavu.9cb1a7f9.png"
+  },
+  {
+    "revision": "72eb90442dff1a4430c29f012ec79ab7",
+    "url": "/personal-website/static/media/enlightenment-vest-2.72eb9044.jpg"
+  },
+  {
+    "revision": "31fafa1b1b7b1f4296e22147107cf63a",
+    "url": "/personal-website/static/media/enlightenment-vest.31fafa1b.jpg"
+  },
+  {
+    "revision": "37ce81e620242f574945ed0efbacc1c2",
+    "url": "/personal-website/static/media/github.37ce81e6.svg"
+  },
+  {
+    "revision": "34ae5992abcf23994b0c0e140262b861",
+    "url": "/personal-website/static/media/googlescholar.34ae5992.svg"
+  },
+  {
+    "revision": "da781403aba821c442fd862dbf7c4bbd",
+    "url": "/personal-website/static/media/infraredtags-thumbnail.da781403.png"
+  },
+  {
+    "revision": "e5c141a8119d270edd908dfc682a3524",
+    "url": "/personal-website/static/media/leiserchess.e5c141a8.png"
+  },
+  {
+    "revision": "23b4eb7dc7099b21409bbc0cb6360aee",
+    "url": "/personal-website/static/media/lenticular-thumbnail.23b4eb7d.jpeg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/personal-website/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "b6c1097f131435ce690255f846a69703",
+    "url": "/personal-website/static/media/lonestar.b6c1097f.png"
+  },
+  {
+    "revision": "49f5e3c1493853ca2e95a890baed87e0",
+    "url": "/personal-website/static/media/morphsensor-thumbnail.49f5e3c1.png"
+  },
+  {
+    "revision": "3c90de636f737379ca6c1f479f54f72b",
+    "url": "/personal-website/static/media/mosaic-stool.3c90de63.jpg"
+  },
+  {
+    "revision": "c2cbc58740be7e3d8f5b3957b5082561",
+    "url": "/personal-website/static/media/profile2.c2cbc587.jpeg"
+  },
+  {
+    "revision": "3e402c9b93f2d9cd67f74a55cb4a92f2",
+    "url": "/personal-website/static/media/shape-grammar-origami.3e402c9b.jpg"
+  },
+  {
+    "revision": "b31c9fdb67e27dd592e9b1b958b92285",
+    "url": "/personal-website/static/media/shaper-sketch.b31c9fdb.png"
+  },
+  {
+    "revision": "e9a66ab3fca46731e54890ba179fcc34",
+    "url": "/personal-website/static/media/text-weather.e9a66ab3.png"
+  }
+]);
