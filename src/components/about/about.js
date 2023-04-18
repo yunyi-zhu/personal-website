ffad94cc3aec66338e9aeb7b0bb25a7bd7b4a5a4
Yunyi Zhu <yunyizhu@mit.edu>
2023-04-18 17:39:40 -0400

    index

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -12,7 +12,7 @@ const About = () => (
       <div className="paragraph">
         I am a PhD student in the <a href="https://hcie.csail.mit.edu/" target="_blank">HCI Engineering Group</a> at <a href="https://www.csail.mit.edu/" target="_blank">MIT CSAIL</a>, advised by <a href="https://hcie.csail.mit.edu/stefanie-mueller" target="_blank">Prof. Stefanie Mueller</a>. 
 
-        My research explores fabrication methods for objects with dynamic appearances, including using optic structures and programmable materials. 
+        My research explores fabrication methods for objects with dynamic appearances, including using compliant structures, optic structures and programmable materials. 
 
         I am a 2021 <a href="https://oge.mit.edu/fellowships/presidential-graduate-fellowship-program/" target="_blank">Jacobs Presidential Fellow</a> previously a visiting student in the <a href="https://www.akg.t.u-tokyo.ac.jp/" target="_blank">Kawahara Lab</a> working with <a href="https://narumi.me/" target="_blank">Prof. Koya Narumi</a>.  
       </div>
