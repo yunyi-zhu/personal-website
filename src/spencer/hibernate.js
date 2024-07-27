a7f7b02e031042e36822a96ba4136b690aed5505
Yunyi Zhu <yunyizhu@mit.edu>
2024-07-27 00:23:38 -0400

    cat appear when clicked

--- src/spencer/hibernate.js
+++ src/spencer/hibernate.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 import restingSeal from './sprites/seal-sleeping.jpg';
 import upSeal from './sprites/seal-up.jpg';
+import catImage from './sprites/cat.png';
 import './css/styles.css';
 
 const HibernationPage = () => {
   const [isResting, setIsResting] = useState(false);
+  const [cats, setCats] = useState([]);
 
   const toggleRest = () => {
     setIsResting(!isResting);
   };
 
+  const handleClick = (e) => {
+    if (!e.target.classList.contains('rest-button')) {
+      const rect = e.currentTarget.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      setCats([...cats, { x, y }]);
+    }
+  };
+
   return (
-    <div className={`hibernation-page ${isResting ? 'resting' : ''}`}>
+    <div className={`hibernation-page ${isResting ? 'resting' : ''}`} onClick={handleClick}>
       <div className="seal-status">
         {isResting ? (
           <div>
@@ -28,6 +39,9 @@ const HibernationPage = () => {
       <button onClick={toggleRest} className="rest-button">
         {isResting ? 'Wake up' : 'Take a rest'}
       </button>
+      {cats.map((cat, index) => (
+        <div key={index} className="cat" style={{ top: cat.y, left: cat.x }}></div>
+      ))}
     </div>
   );
 };
